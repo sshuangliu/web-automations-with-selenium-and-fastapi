264e132d173fef8062db9f93f317fef998992be3
sshuangliu <38092313+sshuangliu@users.noreply.github.com>
2021-10-23 14:47:08 +0800

    dockerfile

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+tiangolo/uvicorn-gunicorn-fastapi:python3.8
+
+LABEL maintainer="Sebastian Ramirez <tiangolo@gmail.com>"
+
+COPY requirements.txt /tmp/requirements.txt
+RUN pip install --no-cache-dir -r /tmp/requirements.txt
+
+COPY ./app /app
---
+++ app/main.py
@@ -0,0 +1,178 @@
+from datetime import datetime, timedelta
+from typing import Optional
+
+from fastapi import Depends, FastAPI, HTTPException, status, Body
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+
+from passlib.context import CryptContext
+
+from pydantic import BaseModel
+import selenium_timesheet
+
+
+# https://fastapi.tiangolo.com/tutorial/security/oauth2-jwt/
+# to get a string like this run in linux:
+# openssl rand -hex 32
+SECRET_KEY = "c4af8692b37bcf2d575c5958254eee21a049cf01925207beb8e4f02a5c0c9593"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+# secret01
+fake_users_db = {
+    "sam": {
+        "username": "sam",
+        "full_name": "sam liu",
+        "email": "sam@example.com",
+        "hashed_password": "$2b$12$X8w2pubd67dP8JsAijamkejzK1LIiY.JMTh7qAgscb9TVkSHkT0sy",
+        "disabled": False,
+    }
+}
+# definition the client's request data model
+class Timesheet(BaseModel):
+    username: str
+    MFAtoken: str
+    addtion: Optional[str] = None
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# state this url-"\token" is used for get token only
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app = FastAPI()
+
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+# used for generating hash_password by plain_password
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password): # the former password is a plain,yes!!
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    '''
+    this function is used for verifying the token's expire time and get the user's infor
+    '''
+    print(token)
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM]) 
+        '''
+        # A token can be decode to be as the data was encoded before
+        if the token has expired then it can not be decode and raise a 
+        'ExpiredSignatureError('Signature has expired.')' error.
+        payload = {'sub': 'johndoe', 'exp': 1634789405}
+        '''
+        print(payload)
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except Exception as e:
+        print(repr(e))
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+
+
+
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+
+@app.post("/timesheet/", dependencies=[Depends(get_current_active_user)])
+def timesheet_app(user_infor: Timesheet = Body(...)): 
+    '''
+    # If you are using a third party library that communicates with something and doesn't have support or dno't know for using await
+    # (this is currently the case for most database libraries), then declare your path operation functions as normally, with just def
+    '''
+    print(user_infor.dict())
+    result = selenium_timesheet.selenium_timesheet_instance(username = user_infor.username, token=user_infor.MFAtoken)
+    return {"message": result }
+
+
+if __name__ == '__main__':
+    pass
+    #import uvicorn
+    #uvicorn.run(app='fastapi_token_demo:app', host="127.0.0.1", port=8000, reload=True, debug=True)
+    #python -m uvicorn fastapi_token_demo:app --host '127.0.0.1' --port 8000 --reload
